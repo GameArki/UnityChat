f90cb568da3bf04a8b203fdafd12aca88642dc27
Jackutea <chenwansal1@163.com>
2024-04-12 17:48:26 +0800

    <feature> add: Send Sample

--- Assets/Scripts_Sample/UnityChatSample.cs
+++ Assets/Scripts_Sample/UnityChatSample.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEditor.PackageManager;
 
 namespace UnityChat.Sample {
 
     public class UnityChatSample : MonoBehaviour {
 
         void Start() {
-            Debug.Log("Hello World");
+
+            const string DOMAIN = "http://localhost:5294";
+            string id = UnityEditor.CloudProjectSettings.userId;
+            string userName = UnityEditor.CloudProjectSettings.userName;
+            Debug.Log(id + " : " + userName);
+
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes("Hello, UnityChat!");
+            UnityWebRequest request = UnityWebRequest.Put(DOMAIN + "/chat/send", bodyRaw);
+            var res = request.SendWebRequest();
+            while (!res.isDone) {
+            }
+            if (request.responseCode != 200) {
+                Debug.LogError("Error: " + request.responseCode);
+            }
+
         }
 
         void Update() {
