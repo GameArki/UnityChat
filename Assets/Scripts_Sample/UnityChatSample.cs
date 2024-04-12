611edde35af6884e4b19cbba7dfefb16db6d49c5
Jackutea <chenwansal1@163.com>
2024-04-12 16:02:43 +0800

    ESTABLISH

---
+++ Assets/Scripts_Sample/UnityChatSample.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityChat.Sample {
+
+    public class UnityChatSample : MonoBehaviour {
+
+        void Start() {
+            Debug.Log("Hello World");
+        }
+
+        void Update() {
+
+        }
+
+    }
+}
